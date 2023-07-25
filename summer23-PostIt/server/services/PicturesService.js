547936621ce80ff660041ede43696aa7eaabf9fa
savtemp <savtemple@gmail.com>
2023-07-25 10:09:52 -0600

    Notes in the picturesService

--- summer23-PostIt/server/services/PicturesService.js
+++ summer23-PostIt/server/services/PicturesService.js
@@ -9,6 +9,11 @@ class PicturesService {
   }
 
   async createPicture(pictureData) {
+    // NOTE find method takes in an object, just based on how the method works
+    // NOTE find pictures where theres albumIds and only give back the pictures that have that albumId
+    // Same as doing this Javascript ---> pictures.filter(p => p.albumId == albumId)
+
+    // NOTE you can also do it this way ---> const pictures = await dbContext.Pictures.find({ albumId: id })
     const picture = await dbContext.Pictures.create(pictureData)
     await picture.populate('creator', 'name picture')
     return picture
